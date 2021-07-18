f873ebe2f2e676431b7f6541d8909762a6c71858
znuznu <znu_1@protonmail.com>
2021-07-18 14:56:29 +0200

    chore: setup

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A task manager made with **React**, **Redux**, **rbd**, **redux-persist**, **Chakra UI** and **Twemoji**.
 
-You can try the project [here](https://znuznu.github.io/toodou).
+You can try the project [here](https://toodou.vercel.app/).
 
 - Create boards containing task lists and tasks
 - Edit the title and content of all entities
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
-import { Box, useColorModeValue } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
 import TaskListHeader from './TaskListHeader';
 import TaskListContent from './TaskListContent';
 
 const TaskList = (props) => {
   const { id, boardId, isNew } = props;
-
-  // Background of the taskList, need one due to the dnd
-  const bg = useColorModeValue('white', 'sith.900');
 
   return (
     <Box
@@ -23,8 +20,6 @@ const TaskList = (props) => {
       pt="0"
       mr="4"
       display="inline-table"
-      // boxShadow="5px 5px 10px #1d1d1d, -5px -5px 10px #272727"
-      // bg={bg}
     >
       <TaskListHeader id={id} boardId={boardId} isNew={isNew} />
       <TaskListContent id={id} />
